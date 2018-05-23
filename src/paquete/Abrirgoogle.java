79fe89430b62988a2ccea456ce62aa869a8f239b
Edgardo César Silva <esilva@intranet.certant.com>
2018-05-23 17:38:03 -0300

    primer commit

---
+++ src/paquete/Abrirgoogle.java
@@ -0,0 +1,17 @@
+package paquete;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Abrirgoogle {
+	public static void main(String[] args){
+		  System.setProperty("webdriver.gecko.driver", "E:\\_Archivos Estables\\Programas\\para java del driver de firefox201\\geckodriver.exe");
+
+		
+		WebDriver driver=new FirefoxDriver();
+		
+		driver.get("http://www.google.com");
+		
+		
+	}
+}
